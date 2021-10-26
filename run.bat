a217e10d3625d9f9a738d99e875803ea7487e27f
biswadip <bisroy15@gmail.com>
2021-10-26 15:39:56 -0700

    third Commit

--- run.bat
+++ run.bat
@@ -1,6 +1,3 @@
 
-C:\Biswadip\Automation\JackWUS\venv\Scripts\activate && ^
-pytest -s -v -m "sanity" && ^
-C:\Biswadip\Automation\JackWUS\venv\Scripts\deactivate
 
-echo Hi
+pytest -s -v -m "sanity" 
